33252510aa3cad68221be83bd2f5aa3c67827da5
Uray Irvan <urayirvan@gmail.com>
2021-07-04 01:16:15 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -23,6 +32,13 @@ const init = async () => {
     options: {
       service: songsService,
       validator: SongsValidator,
+    },
+  },
+  {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
     },
   });
 

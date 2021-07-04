9385d7ac7afcb6ae4508dad7833851d2844f1c08
Uray Irvan <urayirvan@gmail.com>
2021-07-04 21:23:57 +0700

    authentication implemented

--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
+    options: {
+      auth: 'musicapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
+    options: {
+      auth: 'musicapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/songs/{songId}',
     handler: handler.getSongByIdHandler,
+    options: {
+      auth: 'musicapp_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/songs/{songId}',
     handler: handler.putSongByIdHandler,
+    options: {
+      auth: 'musicapp_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/songs/{songId}',
     handler: handler.deleteSongByIdHandler,
+    options: {
+      auth: 'musicapp_jwt',
+    },
   },
 ];
 
